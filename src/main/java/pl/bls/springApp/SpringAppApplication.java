0c66b994d391ed7a15226bfde80b3ee81fcccdcd
blaise <exploit@o2.pl>
2018-05-20 18:02:32 +0200

    first commit

---
+++ src/main/java/pl/bls/springApp/SpringAppApplication.java
@@ -0,0 +1,12 @@
+package pl.bls.springApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppApplication.class, args);
+	}
+}
